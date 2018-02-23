58019b374b17bb952284e11e4a067a39ad141b9b
Daniel Mack <dma2014@outlook.de>
2018-02-23 14:47:54 +0100

    Projektdateien hinzufügen.

---
+++ Dedup/ComparableByteArrayAndSize.vb
@@ -0,0 +1,24 @@
+﻿Imports Dedup
+
+Public Class ComparableByteArrayAndSize
+    Implements IComparable(Of ComparableByteArrayAndSize)
+
+    Private array As Byte()
+    Private size As Long
+
+    Public Sub New(array As Byte(), size As Long)
+        Me.array = array
+        Me.size = size
+    End Sub
+
+
+    Public Function CompareTo(other As ComparableByteArrayAndSize) As Integer Implements IComparable(Of ComparableByteArrayAndSize).CompareTo
+        Return IIf(Me.SequenceEqual(other), 0, Me.size.CompareTo(other.size))
+    End Function
+
+
+    Public Function SequenceEqual(other As ComparableByteArrayAndSize) As Boolean
+        Return array.SequenceEqual(other.array)
+    End Function
+
+End Class
---
+++ Dedup/MainWindow.xaml.vb
@@ -0,0 +1,92 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography
+Imports System.Threading
+Imports System.Windows.Forms
+Imports C5
+
+Class MainWindow
+
+    Private ReadOnly hashes As New TreeDictionary(Of ComparableByteArrayAndSize, String)
+    Private ReadOnly md5 As MD5 = MD5.Create
+    Private Shared ReadOnly remainingFiles As New Queue(Of String)
+    Private Shared ReadOnly waitHandle As New EventWaitHandle(False, EventResetMode.AutoReset)
+    Private Shared exitSignal As Boolean = False
+
+
+    Private Sub browseClicked() Handles btnFolderBrowse.Click
+
+        Dim dlg As New FolderBrowserDialog()
+        dlg.ShowNewFolderButton = False
+
+
+        dlg.ShowDialog()
+
+        ThreadPool.QueueUserWorkItem(Sub() mainThread(dlg.SelectedPath))
+
+    End Sub
+
+    Private Sub mainThread(path As String)
+        Dispatcher.Invoke(Sub() progress.IsIndeterminate = True)
+        Console.WriteLine("generating queue ...")
+        generateQueue(path)
+        Console.WriteLine("queue created!")
+        Dispatcher.Invoke(Sub() progress.IsIndeterminate = False)
+        Console.WriteLine("Starting first task ...")
+        Task.Factory.StartNew(Sub() calcMd5(remainingFiles.Dequeue()))
+
+        While (False = exitSignal And waitHandle.WaitOne())
+            'Console.WriteLine("next thread ...")
+            Task.Factory.StartNew(Sub() calcMd5(remainingFiles.Dequeue()))
+        End While
+
+    End Sub
+
+    Private Sub generateQueue(path As String)
+
+        Dim files As IEnumerable(Of String) = Directory.EnumerateFiles(path)
+
+        Console.WriteLine("detected " & files.Count & " files in selected directory")
+        Dispatcher.Invoke(Sub()
+                              progress.Value = 0
+                              progress.Maximum = files.Count
+                          End Sub)
+
+        Dim i As Integer = 1
+        For Each file As String In files
+            Console.WriteLine("queueing file no. " & i)
+            If (remainingFiles.Count > 0) Then
+                remainingFiles.Enqueue(file)
+            Else
+                remainingFiles.Enqueue(file)
+            End If
+
+            i += 1
+        Next
+    End Sub
+
+
+
+    Private Sub calcMd5(path As String)
+        Dim stream As FileStream = File.Open(path, FileMode.Open)
+        Dispatcher.Invoke(Sub() labelFilename.Content = IO.Path.GetFileName(path))
+        Dim hash As New ComparableByteArrayAndSize(md5.ComputeHash(stream), stream.Length)
+        Dim delete As Boolean = False
+        If (False = hashes.Contains(hash)) Then
+            hashes.Add(hash, path)
+        Else
+            Console.WriteLine("DUPLICATE DETECTED : " & IO.Path.GetFileName(path))
+            delete = True
+
+        End If
+
+        Dispatcher.Invoke(Sub() progress.Value += 1)
+        stream.Close()
+        If delete = True Then
+            File.Delete(path)
+            Console.WriteLine("... deleted")
+        End If
+        waitHandle.Set()
+    End Sub
+
+
+End Class
